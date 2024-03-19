4fef4d4184abd267f3ee6409ac9cc124e9cebd0d
adir1 <aa7381@bs.amalnet.k12.il>
2024-03-19 18:03:18 +0200

    adir

--- app/src/main/java/com/example/mission12/MainActivity.java
+++ app/src/main/java/com/example/mission12/MainActivity.java
@@ -3,18 +3,31 @@ package com.example.mission12;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btn, btn2;
     ImageView iV, iV2, iV3, iV4;
     EditText eT;
+    double num =0;
+    double sum = 0;
 
-    int sum = 0;
+    int count = 0 , countminus = 0 , countkefel = 0 ,counthelok = 0;
+    boolean pluspress = false ;
+
+    boolean minuspress = false ;
+
+    boolean kefelpress = false ;
+
+    boolean helokpress = false ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +40,118 @@ public class MainActivity extends AppCompatActivity {
         iV3 = (ImageView) findViewById(R.id.iV3);
         iV4 = (ImageView) findViewById(R.id.iV4);
         eT = (EditText) findViewById(R.id.eT);
+
+
+
+
+
     }
 
-    public void clicked(View view) {
+    // פלוס) {
 
-        if (eT.getText().toString().isEmpty()) {
+
+        public void clicked (View view)
+        {
+            pluspress = true;
+
+            String textValue = eT.getText().toString();
+            count++;
+            num = Double.parseDouble(textValue);
+
+            if (count>=2)
+            {
+                sum+=num;
+                eT.setText(String.valueOf(sum));
+                count = 0 ;
+            } else if (!eT.getText().toString().isEmpty()) {
+                eT.setText("");
+                sum=num;
+            }
+        }
+
+    //מינוס
+    public void clicked2(View view)
+    {
+        minuspress = true;
+
+        String textValue2 = eT.getText().toString();
+        count++;
+        num = Double.parseDouble(textValue2);
+
+        if (count>=2)
+        {
+            sum-=num;
+            eT.setText(String.valueOf(sum));
+            count = 0 ;
+        } else if (!eT.getText().toString().isEmpty()) {
             eT.setText("");
-            
+            sum=num;
         }
     }
+
+
+    //כפל
+    public void clicked3(View view)
+    {
+        kefelpress = true;
+
+        String textValue3 = eT.getText().toString();
+        count++;
+        num = Double.parseDouble(textValue3);
+
+        if (count>=2)
+        {
+            sum*=num;
+            eT.setText(String.valueOf(sum));
+            count = 0 ;
+        } else if (!eT.getText().toString().isEmpty()) {
+            eT.setText("");
+            sum=num;
+        }
+    }
+    public void equal(View view)
+    {
+        if(pluspress == true)
+        {
+            count = 0 ;
+            pluspress = false;
+            String textValue = eT.getText().toString();
+            num = Double.parseDouble(textValue);
+            sum+=num;
+        }
+        if(minuspress == true)
+        {
+            count = 0 ;
+            minuspress = false;
+            String textValue2 = eT.getText().toString();
+            num = Double.parseDouble(textValue2);
+            sum-=num;
+        }
+        if(kefelpress == true)
+        {
+            count = 0 ;
+            kefelpress = false;
+            String textValue3 = eT.getText().toString();
+            num = Double.parseDouble(textValue3);
+            sum*=num;
+        }
+        eT.setText(String.valueOf(sum));
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
